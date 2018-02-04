093ac45f9814067086ba7b25663eb00e9e315bf4
Aishwarya Sundararajan <asundar2@ncsu.edu>
2018-02-04 17:00:33 -0500

    Integrating mongoose and mongoDB

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express= require('express');
+const mongoose=require('mongoose');
 const app=express();
+// Map global promise- get rid of warning
+mongoose.Promise= global.Promise;
+
+// Connect to mongoDB
+mongoose.connect('mongodb://localhost/roommate-dev',{
+  useMongoClient: true
+}).then(()=> console.log('MongoDB connected')).catch(err=>console.log(err));
 
 // Index route
 app.get('/',(req,res)=>{
