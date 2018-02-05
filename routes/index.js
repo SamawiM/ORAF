7f74ec742e7783249868dd557911f36939c67c4a
Arpita Shekhar <arpitashekhar@Arpitas-MacBook-Pro.local>
2018-02-05 00:11:03 -0500

    Adding new index file for routes and partials for header

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* render index page */
+router.get('/', function(req,res,next) {
+	res.render('index', { title: 'Roommate Finder Application'});
+});
+
+// Edit-profile page
+app.get('/editprofile',(req,res)=>{
+ res.render('editprofile');
+});
+
+// Review-roomie page
+app.get('/reviewroomie',(req,res)=>{
+ res.render('reviewroomie');
+});
+
+module.exports = router;
