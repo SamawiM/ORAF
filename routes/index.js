85a68bc6e365c724e2ff676cf961642458a3e7f9
Arpita Shekhar <arpitashekhar90@gmail.com>
2018-02-17 14:06:20 -0500

    removing comments

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,4 @@
-// const routes = require('express').Router();
 
-// // Default route
-// routes.get('/',(req, res)=>{
-//    res.render('helloworld');
-// });
-
-// // Search page
-// routes.get('/search/search',(req,res)=>{
-//   res.render('search/search');
-// });
-
-// module.exports = routes;
 const login = require('../app/controllers/home');
 module.exports = function (app) {
 	app.get('/', login.index);
