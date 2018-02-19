e8a35fb17a769e9c6e3b4600123e9234cc003363
Aishwarya <aishwaryassr@gmail.com>
2018-02-18 23:21:28 -0500

    Email verification functionality enabled

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,12 @@
+var nodemailer=require("nodemailer");
+var smtpTransport=nodemailer.createTransport({
+	service: "gmail",
+	auth:{
+		user: "aishwaryassr@gmail.com",
+		pass: "Reddeogre.123"
+	}
+});
+var rand,mailOptions,host,link;
 
 const login = require('../app/controllers/home');
 module.exports = function (app,User,mongoose,session) {
@@ -31,11 +40,28 @@ module.exports = function (app,User,mongoose,session) {
 		});
 	}else{
 		console.log('success signup')
-		const newUser={
-			email: req.body.signupEmail,
-		}
-		new User(newUser).save();
-		res.render('login/signupsuccess')
+
+	//smtp logic
+	rand=Math.floor((Math.random()*100)+54);
+	host=req.get('host');
+	link="http://"+req.get('host')+"/verify?id="+rand;
+	mailOptions={
+		to: req.body.signupEmail+"@ncsu.edu",
+		subject: "Please confirm your email account",
+		html: "Hello,<br> Please click and verify<br><a href="+link+">Click here to verify</a>"
+	}
+ console.log(mailOptions);
+smtpTransport.sendMail(mailOptions,(error,response)=>{
+ if(error)
+ {
+	 console.log(error);
+	res.end("error"); 
+ }else{
+	 console.log("Message sent:"+response.message);
+	 res.end("sent");
+ }
+});
+
 	}
 	 });
 	 app.post('/signin',(req,res)=>{
@@ -54,5 +80,32 @@ module.exports = function (app,User,mongoose,session) {
 				console.log('Signin failure');
 			}
 		});
+	})
+	app.get('/verify',(req,res)=>{
+		console.log(req.protocol+":/"+req.get('host'));
+		if((req.protocol+"://"+req.get('host'))==("http://"+host))
+{
+    console.log("Domain is matched. Information is from Authentic email");
+    if(req.query.id==rand)
+    {
+        console.log("email is verified");
+				res.end("<h1>Email "+mailOptions.to+" is been Successfully verified");
+				const newUser={
+					email: req.body.signupEmail,
+				}
+				new User(newUser).save();
+				res.render('login/signupsuccess')
+    }
+    else
+    {
+        console.log("email is not verified");
+        res.end("<h1>Bad Request</h1>");
+    }
+}
+else
+{
+    res.end("<h1>Request is from unknown source");
+}
+
 	})
 }
